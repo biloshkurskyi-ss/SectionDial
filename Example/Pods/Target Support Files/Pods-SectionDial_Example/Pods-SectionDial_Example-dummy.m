c7748f4a542c8192c14894e5e51dcf447fd077b5
Serhii <serhiib@gmedinnovations.com>
2018-11-27 12:32:59 +0200

    Create a lib.

---
+++ Example/Pods/Target Support Files/Pods-SectionDial_Example/Pods-SectionDial_Example-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_SectionDial_Example : NSObject
+@end
+@implementation PodsDummy_Pods_SectionDial_Example
+@end
---
+++ Example/Pods/Target Support Files/SectionDial/SectionDial-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_SectionDial : NSObject
+@end
+@implementation PodsDummy_SectionDial
+@end
